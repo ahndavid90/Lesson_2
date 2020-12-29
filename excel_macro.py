c6ed80e079267261d626fed2b42eefbbe0302333
ahndavid90 <ahndavid90@gmail.com>
2020-12-29 00:04:32 -0500

    Test

--- excel_macro.py
+++ excel_macro.py
@@ -4,6 +4,7 @@ import os
 
 def automation():
     print('start!')
+    print('test')
     original_path = os.path.join(os.getcwd(),'Summary_12-23-2020.xlsx')
     rates_path = os.path.join(os.getcwd(), 'Inputs_12-24-2020.xlsx')
 
