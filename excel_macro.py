46e5a27330da0c0481afdb924262a49dadc5a844
ahndavid90 <ahndavid90@gmail.com>
2021-01-01 11:51:43 -0500

    Change1

--- excel_macro.py
+++ excel_macro.py
@@ -3,9 +3,6 @@ import openpyxl
 import os
 
 def automation():
-    print('start!')
-    print('test')
-    print('test3')
     original_path = os.path.join(os.getcwd(),'Summary_12-23-2020.xlsx')
     rates_path = os.path.join(os.getcwd(), 'Inputs_12-24-2020.xlsx')
 
